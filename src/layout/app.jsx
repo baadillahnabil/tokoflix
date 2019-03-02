c4fcca1a5ab798e9cbb4547e605f9cdd42d2d27d
baadillahnabil <nabilzero27@gmail.com>
2019-03-02 18:37:09 +0700

    add default balance and pass it as context API

--- src/layout/app.jsx
+++ src/layout/app.jsx
@@ -8,25 +8,46 @@ import Classes from './app.module.scss'
 import Home from '../pages/Home/home'
 import Detail from '../pages/Detail/detail'
 
+import { Provider } from '../contextApi/context'
+
 class App extends Component {
+  state = {
+    balance: 100000,
+  }
+
   render() {
     return (
-      <div className={Classes.rootApp}>
-        <AppBar className={Classes.navbar} position="fixed" color="default">
-          <Toolbar>
-            <p className={Classes.navbarTitle}>Tokoflix</p>
-            <p className={Classes.balance}>
-              Rp <span className={Classes.balanceAmount}>200000</span>
-            </p>
-          </Toolbar>
-        </AppBar>
-        <div className={Classes.rootPages}>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/:movie_id/:slug" component={Detail} />
-          </Switch>
+      <Provider
+        value={{
+          state: this.state,
+          actions: {
+            updateBalance: newBalance =>
+              this.setState({
+                balance: newBalance,
+              }),
+          },
+        }}
+      >
+        <div className={Classes.rootApp}>
+          <AppBar className={Classes.navbar} position="fixed" color="default">
+            <Toolbar>
+              <p className={Classes.navbarTitle}>Tokoflix</p>
+              <p className={Classes.balance}>
+                Rp{' '}
+                <span className={Classes.balanceAmount}>
+                  {this.state.balance}
+                </span>
+              </p>
+            </Toolbar>
+          </AppBar>
+          <div className={Classes.rootPages}>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/:movie_id/:slug" component={Detail} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Provider>
     )
   }
 }
