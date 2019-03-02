03ff0b687aee27cf0181dd22eafe61cf8fc5bc4d
baadillahnabil <nabilzero27@gmail.com>
2019-03-02 16:33:22 +0700

    add env file for stroing global variable

---
+++ src/services/env.js
@@ -0,0 +1,8 @@
+// Base Url for the API Request
+export const BASE_URL = 'https://api.themoviedb.org/3/'
+
+// Key for request to API
+export const API_KEY = '81eb02a0cbd582697702eff26e790972'
+
+// Base Url for Images from API
+export const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/original'
