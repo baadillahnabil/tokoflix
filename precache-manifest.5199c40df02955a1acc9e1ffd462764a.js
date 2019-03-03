d492aaf11beaa0ad9b787fd9734da5aa25c9a9be
baadillahnabil <nabilzero27@gmail.com>
2019-03-03 15:23:39 +0700

    Updates

---
+++ precache-manifest.5199c40df02955a1acc9e1ffd462764a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c383f79fbb49dff036b9",
+    "url": "/tokoflix/static/css/main.ba65c1e8.chunk.css"
+  },
+  {
+    "revision": "c383f79fbb49dff036b9",
+    "url": "/tokoflix/static/js/main.c383f79f.chunk.js"
+  },
+  {
+    "revision": "0ae4abe108bd6b4a2288",
+    "url": "/tokoflix/static/js/runtime~main.0ae4abe1.js"
+  },
+  {
+    "revision": "562b4838a5eda8ea843e",
+    "url": "/tokoflix/static/js/2.562b4838.chunk.js"
+  },
+  {
+    "revision": "0576a8de0a5ef7917dcc1d601ffa6533",
+    "url": "/tokoflix/index.html"
+  }
+];
